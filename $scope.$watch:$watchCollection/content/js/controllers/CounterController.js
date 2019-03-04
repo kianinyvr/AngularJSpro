f62d70e01d727e98b245bae60aa18e3dbd6900e4
kakhavan <kian.akhavan@gmail.com>
2019-03-04 14:07:16 -0800

    added to observe changes

--- $scope.$watch:$watchCollection/content/js/controllers/CounterController.js
+++ $scope.$watch:$watchCollection/content/js/controllers/CounterController.js
@@ -1,18 +1,24 @@
 function CounterController ($scope) {
     this.count = 44;
+    this.countList = [];
 
     this.increment = function increment(){
         this.count++;
+        this.countList.unshift({id: this.count});
     };
 
     this.decrement = function decrement(){
         this.count--;
+        this.countList.unshift({id: this.count});
     };
 
-    $scope.$watch(function() {
-        return this.count;
+    $scope.$watchCollection(function() {
+        return this.countList;
     }.bind(this), function(newValue, oldValue) {
-        console.log(newValue, oldValue);
+        if (newValue === oldValue) {
+            return;
+        }
+        console.log("New: ", newValue, "Old: ", oldValue);
     });
 }
 
